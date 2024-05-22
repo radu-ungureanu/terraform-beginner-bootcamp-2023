5d049560841de000704efe432bbb0b2defe00de8
Radu Ungureanu <radu.ungureanu@outlook.com>
2024-05-22 13:21:04 +0300

    Add CloudFront as a distribution mechanism for the website files.

---
+++ modules/terrahouse_aws/resource_storage.tf
@@ -0,0 +1,58 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    UserUUID = var.user_uuid
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "website_index" {
+  bucket       = aws_s3_bucket.website_bucket.bucket
+  key          = "index.html"
+  content_type = "text/html"
+  source       = var.index_html_filepath
+  etag         = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "website_error" {
+  bucket       = aws_s3_bucket.website_bucket.bucket
+  key          = "error.html"
+  content_type = "text/html"
+  source       = var.error_html_filepath
+  etag         = filemd5(var.error_html_filepath)
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action   = "s3:GetObject",
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+          }
+        }
+      }
+    ]
+  })
+}
